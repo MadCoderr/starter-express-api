134836631aa4b3ad1c72d07fa34694c47768534e
MadCoderr <msrfarooqi@gmail.com>
2022-10-20 16:10:27 +0500

    feat: post request

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Yo!");
+});
+
+app.post("/create-user", (req, res) => {
+  const { username, email } = req.body;
+
+  res.status(200).json({ username, email });
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () =>
+  console.log(`listening on PORT http://localhost:${PORT}`)
+);
